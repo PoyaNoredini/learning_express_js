8074e42672bfb2b511fb63ac7b25fedde2cd6a57
poyanoredini <poya.ali.m@gmail.com>
2025-04-23 10:50:17 +0000

    update error  controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,44 @@
+   const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error : err,
+        message: err.message,
+        stack : err.stack
+    });
+   }
+   
+   const sendErrorProd = (err, res) => {
+    if (err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+             });
+    }else{
+        //log errpr 
+        console.error('error' , err);
+
+        //send generic error
+        res.status(500).json({
+            status: 'error',
+            message: 'something went wrong' 
+        })
+
+    }
+}
+
+
    module.exports = (err , req , res , next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
     
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });   
+    if(process.env.NODE_ENV === 'development'){
+
+        sendErrorDev(err,res);
+        
+    }else if (process.env.NODE_ENV === 'production'){
+    sendErrorProd(err,res);
+    
+    }
+
    }
     
