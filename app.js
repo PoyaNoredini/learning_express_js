a21f18f4019afd9dd6c4b3c994307730a4101e35
poyanoredini <poya.ali.m@gmail.com>
2025-04-24 13:39:32 +0000

    update app

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
-const tourRouter = require('./routes/toureRouter');
-// const userRouter = require('./routes/userRoutes');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-const app = express();
-app.use(express.json());
+const tourRouter = require('./routes/toureRouter');
 
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 
 
 app.all('*', (req, res, next) => {
-    next(new AppError(`can't find ${req.originalUrl} on this server`,404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
 
 app.use(globalErrorHandler);
 
-// app.use('/api/v1/users', userRouter);
 module.exports = app;
