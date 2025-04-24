c4173505f93eb3e5dc3421a3069ef52d509740fb
poyanoredini <poya.ali.m@gmail.com>
2025-04-24 13:39:41 +0000

    update server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+
 dotenv.config({ path: './.env' });
 
 const app = require('./app');
@@ -14,12 +23,21 @@ mongoose.connect(DB)
     console.log('DB connection successful');
   })
   .catch(err => {
-    console.error('DB connection error:', err);
-  });
+    console.error('DB connection error:', err.message);
+    });
+
 
 const port = process.env.PORT || 3000; // Note: PORT is now uppercase to match your env file
 // console.log(URL)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
